853772c1224a2efa64b41174f66ff9fd5b004eeb
hrolfurinn <106666932+hrolfurinn@users.noreply.github.com>
2023-12-02 23:57:47 -0500

    Inital adaptation for second part of day1
    Created DIGITS constant. Access that when passing through each window of size 5 of the passed in line.

--- 2023/day-1/src/main.rs
+++ 2023/day-1/src/main.rs
@@ -1,8 +1,9 @@
 // use anyhow::Result;
 // use clap::Parser;
 use std::fs::File;
-use std::io::{stdout, BufReader, Cursor, BufRead};
-use std::path::Path;
+// use std::io::{stdout, BufRead, BufReader, Cursor};
+use std::io::{BufRead, BufReader};
+// use std::path::Path;
 
 // #[derive(Parser)]
 // struct Cli {
@@ -12,19 +13,25 @@ use std::path::Path;
 
 // #[derive(Debug)]
 // struct CustomError(String);
+const DIGITS: [&str; 18] = [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
+    "seven", "eight", "nine",
+];
 
-fn main()  -> std::io::Result<()> {
+fn main() -> std::io::Result<()> {
     // env_logger::init();
-
+    
     // log::info!("Starting up.");
     // let args: Cli = Cli::parse();
     let input_path: String = String::from("./input/input.txt");
-    let output_path: String = String::from("./output/output.txt");
-
+    // let output_path: String = String::from("./output/output.txt");
+    
     let f: File = File::open(input_path)?;
     let reader: BufReader<File> = BufReader::new(f);
 
+    
     let mut sum: u32 = 0;
+
 
     // println!("{}", create_number("abc".to_string()));
 
@@ -36,6 +43,10 @@ fn main()  -> std::io::Result<()> {
 
     println!("{sum}");
 
+    println!("testing");
+    let test_num = create_number_with_letters("onetwothreefour".to_string());
+    println!("{test_num}");
+
     Ok(())
 }
 
@@ -46,13 +57,32 @@ fn create_number(line: String) -> u32 {
         if character.is_digit(10) {
             if first_digit == '\0' {
                 first_digit = character;
-            }
-            else {
+                last_digit = character;
+            } else {
                 last_digit = character;
             }
         }
     }
-    10*first_digit.to_digit(10).unwrap_or(0) as u32 + last_digit.to_digit(10).unwrap_or(0) as u32
+    10 * first_digit.to_digit(10).unwrap_or(0) as u32 + last_digit.to_digit(10).unwrap_or(0) as u32
+}
+
+fn create_number_with_letters(line: String) -> u32 {
+    let windows: std::slice::Windows<'_, u8> = line[..].as_bytes().windows(5);
+    println!("{:?}", windows);
+    // convert all byte windows to the equivalent number, based on the digits iterator
+    // filter out the ones that don't map to a number
+    let found_digits = windows.into_iter().filter_map(|byte_slice| convert_to_number(byte_slice));
+    println!("{:?}", found_digits);
+    found_digits.sum()
+}
+
+fn convert_to_number(byte_slice: &[u8]) -> Option<u32>{
+    println!("In covert to number for slice {:?}", byte_slice);
+    let digit_iterators = DIGITS.iter().map(|digit| digit.as_bytes().iter());
+    for digit in digit_iterators {
+        println!("{:?}", digit);
+    }
+    Some(2)
 }
 
 // #[test]
