4214e234b604e80de059023dc5f567a5776ff5f1
hrolfurinn <106666932+hrolfurinn@users.noreply.github.com>
2023-12-06 11:25:45 -0500

    Implemented, erroneous

--- 2023/day-2/src/main.rs
+++ 2023/day-2/src/main.rs
@@ -7,7 +7,7 @@ fn main() -> Result<()> {
     let sample_input_path: String = String::from("./input/sample_input.txt");
 
     // let f: File = File::open(input_path)?;
-    let f: File = File::open(sample_input_path)?;
+    let f: File = File::open(input_path)?;
     let reader: BufReader<File> = BufReader::new(f);
 
     let mut sum1 = 0;
@@ -15,18 +15,43 @@ fn main() -> Result<()> {
 
     let colors = ["red", "green", "blue"];
 
+    let color_count = |color: &str| {
+        for (index, c) in colors.iter().enumerate() {
+            if c.eq(&color) {
+                return Some(index + 12);
+            }
+        }
+        return None;
+    };
+
     for mut line in reader.lines() {
         let line = line?;
         println!("Line: {:?}", line);
-        let mut words: Vec<String> = line.split_whitespace().map(|w| w.to_string()).collect();
-        let mut chunks = words.chunks(2);
-        let id_inner = &chunks.next().unwrap()[1];
-        println!("{:?}", id_inner);
-        let id = id_inner
-            .parse::<u32>();
-        println!("ID: {:?}", id);
-        for chunk in chunks {
-            println!("{:?}", chunk)
+        let mut segments = line.split(":").map(|s| {
+            s.split(";")
+                .map(|p| p.split(",").map(|r| r.split_whitespace()))
+        });
+        let id = segments
+            .next()
+            .unwrap()
+            .next()
+            .unwrap()
+            .next()
+            .unwrap()
+            .collect::<Vec<_>>()[1]
+            .parse::<i64>();
+        let all_correct = segments.next().unwrap().all(|mut s| {
+            let segment_correct = s.all(|mut p| {
+                // println!({"{}"})
+                let is_correct = (p.next().unwrap().parse::<i64>().unwrap_or(0) as usize)
+                    .lt(&color_count(p.next().unwrap()).unwrap());
+                return is_correct;
+            });
+            println!("{:?}", &segment_correct);
+            return segment_correct;
+        });
+        if all_correct {
+            sum1 = sum1 + id.unwrap_or(0) as i64;
         }
     }
 
