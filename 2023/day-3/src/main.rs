d579e65edd0477c94c19708a425dee54b678b006
hrolfurinn <106666932+hrolfurinn@users.noreply.github.com>
2023-12-07 12:47:48 -0500

    Adjacency logic and initial iteration loops

--- 2023/day-3/src/main.rs
+++ 2023/day-3/src/main.rs
@@ -1,6 +1,25 @@
+use core::num;
 use std::fs::File;
 use std::io::{self, BufRead, Result, BufReader};
 use std::str::FromStr;
+
+fn neighbor_check(current_lines: &Vec<String>, index: usize, num_length: usize) -> bool {
+    let start = index - 1;
+    let end = index + num_length;
+    let indices = start..=end;
+    let chars: Vec<Vec<_>> = vec![current_lines[0].chars().collect(), current_lines[1].chars().collect(), current_lines[2].chars().collect()];
+    for ix in indices {
+        if !(chars[0][ix] == '.' && chars[2][ix] == '.') {
+            return true
+        }
+    }
+    if !(chars[1][start] == '.' && chars[1][end] == '.') {
+        return false
+    }
+    else {
+        return false
+    }
+}
 
 fn main() -> Result<()> {
     let input_path = "./input/input.txt";
@@ -15,12 +34,30 @@ fn main() -> Result<()> {
 
     let mut lines = reader.lines();
 
-    let previous_line = lines.next();
-    let current_line = lines.next();
-
-    while let Some(next_line) = lines.next() {
-        println!("{:?}", next_line);
+    let mut current_lines: Vec<String> = Vec::new();
+    for _ in 0..3 {
+        if let Some(Ok(line)) = lines.next() {
+            current_lines.push(line);
+        }
     }
+    for line in &current_lines {
+        println!("{line}");
+    }
+    for line in [&current_lines[1]] {
+        let characters: Vec<_> = line.chars().collect();
+        for (ix, character) in characters.iter().enumerate() {
+            // println!("{:?}, {:?}", ix, character);
+            if character.is_digit(10) {
+                // println!("A digit!");
+                let number: String = characters[ix..].iter().take_while(|c| c.is_digit(10)).collect();
+                if neighbor_check(&current_lines, ix, number.len()) {
+                    println!("{:?}", number);
+                    p1 += number.parse::<i32>().unwrap();    
+                }
+            }
+        }
+    }
+    println!("{p1}");
 
     Ok(())
 }
