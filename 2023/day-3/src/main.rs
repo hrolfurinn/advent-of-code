ccda97a231636b095b49e2a51a90acc1a1797a81
hrolfurinn <106666932+hrolfurinn@users.noreply.github.com>
2023-12-08 12:48:24 -0500

    Part 1 complete, starting part 2

--- 2023/day-3/src/main.rs
+++ 2023/day-3/src/main.rs
@@ -1,5 +1,5 @@
 use core::num;
-use std::collections::VecDeque;
+use std::collections::{VecDeque, vec_deque};
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Result};
@@ -8,7 +8,6 @@ use std::str::FromStr;
 use std::thread::current;
 
 fn neighbor_check(current_lines: &VecDeque<String>, index: usize, num_length: usize) -> bool {
-    env::set_var("RUST_BACKTRACE", "1");
     let start = index - 1;
     let end = index + num_length;
     let indices = start..=end;
@@ -16,23 +15,28 @@ fn neighbor_check(current_lines: &VecDeque<String>, index: usize, num_length: us
         current_lines[0].chars().collect(),
         current_lines[1].chars().collect(),
         current_lines[2].chars().collect(),
-    ];
+        ];
     for ix in indices {
         if !(chars[0][ix] == '.' && chars[2][ix] == '.') {
             return true;
         }
     }
     if !(chars[1][start] == '.' && chars[1][end] == '.') {
-        return false;
+        return true;
     } else {
         return false;
     }
 }
 
+fn ratio_check(current_lines: &VecDeque<String>, index: usize) -> Option<i32> {
+    Some(2)
+}
+
 fn main() -> Result<()> {
+    env::set_var("RUST_BACKTRACE", "1");
     let input_path = "./input/input.txt";
     let sample_input_path = "./input/sample_input.txt";
-    let file = File::open(sample_input_path)?;
+    let file = File::open(input_path)?;
     let reader = BufReader::new(file);
 
     let mut p1 = 0;
@@ -42,22 +46,33 @@ fn main() -> Result<()> {
     // let next_line = reader.lines().next();
 
     let mut lines = reader.lines();
+    let mut first_line = lines.next().unwrap()?;
+
+    macro_rules! pad {
+        ($s:expr) => {
+            format!(".{}.", $s)
+        }
+    }
+
+    let dummy_line: String = ["."].repeat(first_line.len() + 2).concat().into();
 
     let mut current_lines: VecDeque<String> = VecDeque::new();
-    let first_line = for line in &current_lines {
-        println!("{line}");
-    };
-    while let Some(Ok(line)) = lines.next() {
-        println!("{:?}", ".".to_string().repeat(50));
+    current_lines.push_front(dummy_line.clone());
+    current_lines.push_front(pad!(first_line));
+
+    let mut ongoing: bool = true;
+
+    while ongoing {
+        // println!("{:?}", ".".to_string().repeat(50));
         // println!("NEW LINE");
         // println!("{:?}", line);
-        if current_lines.is_empty() {
-            for _ in 0..3 {
-                current_lines.push_back(["."].repeat(line.len() + 2).concat().into());
-            }
+        if let Some(Ok(next_line)) = lines.next() {
+            current_lines.push_front(pad!(next_line));
+        } else {
+            current_lines.push_front(dummy_line.clone());
+            ongoing = false;
         }
-        current_lines.pop_front();
-        current_lines.push_back(".".to_string().add(&line).add("."));
+
         assert_eq!(current_lines.len(), 3);
         let characters: Vec<_> = current_lines[1].chars().collect();
         let mut skip = 0;
@@ -77,18 +92,28 @@ fn main() -> Result<()> {
                 // for cline in &current_lines {
                 //     println!("{:?}", cline);
                 // }
-                println!("Found number {:?}", number);
+                // println!("Found number {:?}", number);
                 skip = number.len() - 1;
                 if neighbor_check(&current_lines, ix, number.len()) {
-                    println!("Adjacent.");
+                    // println!("Adjacent.");
                     p1 += number.parse::<i32>().unwrap();
                 } else {
-                    println!("Not adjacent!")
+                    // println!("Not adjacent!")
+                }
+            }
+            if *character == '*' {
+                if let Some(ratio) = ratio_check(&current_lines, ix) {
+                    p2 += ratio;
+                } else {
+
                 }
             }
         }
+
+        current_lines.pop_back();
     }
-    println!("{p1}");
+    println!("p1 {p1}");
+    println!("p2 {p2}");
 
     Ok(())
 }
