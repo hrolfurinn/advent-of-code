ef1d66fd45f234d93af4c0f56bce0a06c2f75c74
hrolfurinn <106666932+hrolfurinn@users.noreply.github.com>
2023-12-08 13:55:37 -0500

    Formatting and cleanup

--- 2023/day-3/src/main.rs
+++ 2023/day-3/src/main.rs
@@ -1,16 +1,12 @@
-use core::num;
-use std::collections::{VecDeque, vec_deque};
+use std::collections::{VecDeque};
 use std::env;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, Result};
-use std::ops::Add;
-use std::str::FromStr;
-use std::thread::current;
+use std::io::{BufRead, BufReader, Result};
 
 #[derive(Debug)]
 struct PartNumber {
     number: i32,
-    indexes: Vec<usize>
+    indexes: Vec<usize>,
 }
 
 fn neighbor_check(current_lines: &VecDeque<String>, index: usize, num_length: usize) -> bool {
@@ -21,7 +17,7 @@ fn neighbor_check(current_lines: &VecDeque<String>, index: usize, num_length: us
         current_lines[0].chars().collect(),
         current_lines[1].chars().collect(),
         current_lines[2].chars().collect(),
-        ];
+    ];
     for ix in indices {
         if !(chars[0][ix] == '.' && chars[2][ix] == '.') {
             return true;
@@ -37,15 +33,10 @@ fn neighbor_check(current_lines: &VecDeque<String>, index: usize, num_length: us
 fn ratio_check(current_lines: &VecDeque<String>, index: usize) -> Option<i32> {
     let start = index - 1;
     let end = index + 1;
-    let indices = start..=end;
     let mut numbers: Vec<PartNumber> = vec![];
     let mut skip = 0;
-    for line in current_lines {
-        println!("{line}");
-    }
 
     for line in current_lines {
-        println!("Investigating line {line}");
         let characters: Vec<_> = line.chars().collect();
         for (ix, c) in characters.iter().enumerate() {
             if skip != 0 {
@@ -53,11 +44,15 @@ fn ratio_check(current_lines: &VecDeque<String>, index: usize) -> Option<i32> {
                 continue;
             }
             if c.is_digit(10) {
-                println!("Found digit {} at index {}", c, ix);
-                let number: String = characters[ix..].iter().take_while(|c| c.is_digit(10)).collect();
+                let number: String = characters[ix..]
+                    .iter()
+                    .take_while(|c| c.is_digit(10))
+                    .collect();
                 let indexes: Vec<usize> = (ix..ix + number.len()).collect();
-                let part_number = PartNumber { number: number.parse::<i32>().unwrap(), indexes: indexes };
-                println!("Got part number {:?}", &part_number);
+                let part_number = PartNumber {
+                    number: number.parse::<i32>().unwrap(),
+                    indexes: indexes,
+                };
                 numbers.push(part_number);
                 skip = number.len();
             }
@@ -65,44 +60,34 @@ fn ratio_check(current_lines: &VecDeque<String>, index: usize) -> Option<i32> {
     }
     let mut count = 0;
     let mut ratio = 1;
-    println!("Validation");
-    for p_n in &numbers {
-        println!("{:?}", p_n);
-    }
 
     for part_number in &numbers {
         for ix in start..=end {
             if part_number.indexes.contains(&ix) {
-                println!("Valid part number {:?}", part_number);
                 count += 1;
-                ratio *= part_number.number; 
+                ratio *= part_number.number;
                 break;
             }
             if count > 2 {
-                print!("Too many!");
-                return None
+                return None;
             }
         }
     }
     if count < 2 {
-        print!("Too few");
-        return None
+        return None;
     }
-    return Some(ratio)
+    return Some(ratio);
 }
 
 fn main() -> Result<()> {
     env::set_var("RUST_BACKTRACE", "1");
     let input_path = "./input/input.txt";
     let sample_input_path = "./input/sample_input.txt";
-    let file = File::open(sample_input_path)?;
+    let file = File::open(input_path)?;
     let reader = BufReader::new(file);
 
     let mut p1 = 0;
     let mut p2 = 0;
-
-    // let previous_line = reader.lines().next();
-    // let next_line = reader.lines().next();
 
     let mut lines = reader.lines();
     let mut first_line = lines.next().unwrap()?;
@@ -110,7 +95,7 @@ fn main() -> Result<()> {
     macro_rules! pad {
         ($s:expr) => {
             format!(".{}.", $s)
-        }
+        };
     }
 
     let dummy_line: String = ["."].repeat(first_line.len() + 2).concat().into();
@@ -122,9 +107,6 @@ fn main() -> Result<()> {
     let mut ongoing: bool = true;
 
     while ongoing {
-        println!("{:?}", ".".to_string().repeat(50));
-        // println!("NEW LINE");
-        // println!("{:?}", line);
         if let Some(Ok(next_line)) = lines.next() {
             current_lines.push_front(pad!(next_line));
         } else {
@@ -136,36 +118,25 @@ fn main() -> Result<()> {
         let characters: Vec<_> = current_lines[1].chars().collect();
         let mut skip = 0;
         for (ix, character) in characters.iter().enumerate() {
-            // println!("{:?}, {:?}", ix, character);
             if skip != 0 {
                 skip -= 1;
                 continue;
             }
             if character.is_digit(10) {
-                // println!("A digit!");
                 let number: String = characters[ix..]
                     .iter()
                     .take_while(|c| c.is_digit(10))
                     .collect();
-                // println!("In lines:");
-                // for cline in &current_lines {
-                //     println!("{:?}", cline);
-                // }
-                // println!("Found number {:?}", number);
                 skip = number.len() - 1;
                 if neighbor_check(&current_lines, ix, number.len()) {
-                    // println!("Adjacent.");
                     p1 += number.parse::<i32>().unwrap();
                 } else {
-                    // println!("Not adjacent!")
                 }
             }
             if *character == '*' {
-                println!("POTENTIAL GEAR");
                 if let Some(ratio) = ratio_check(&current_lines, ix) {
                     p2 += ratio;
                 } else {
-
                 }
             }
         }
