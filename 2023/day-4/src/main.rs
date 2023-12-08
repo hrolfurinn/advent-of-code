76cd2a0956d2b710f3c3f8c73cac7cfe3efb1a0e
hrolfurinn <106666932+hrolfurinn@users.noreply.github.com>
2023-12-08 14:50:21 -0500

    Day 4 Part 1 Complete

--- 2023/day-4/src/main.rs
+++ 2023/day-4/src/main.rs
@@ -14,5 +14,34 @@ fn main() -> Result<()> {
 
     let mut lines = reader.lines();
 
+    for line in lines {
+        let mut line = line?;
+        let (card_id, numbers) = line.split_once(":").unwrap();
+        let numbers_vec: Vec<_> = numbers
+            .split("|")
+            .map(|s| {
+                s.to_string()
+                    .split_whitespace()
+                    .map(|n| n.to_string().parse::<i32>().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .collect();
+        
+        let matched_numbers = numbers_vec[1]
+            .iter()
+            .filter(|n| numbers_vec[0].contains(n))
+            .collect::<Vec<_>>();
+        if !matched_numbers.is_empty() {
+            p1 += 2_i32.pow(
+                matched_numbers
+                    .len() as u32
+                    - 1,
+            );
+        };
+    }
+
+    println!("p1: {p1}");
+    println!("p2: {p2}");
+
     Ok(())
 }
