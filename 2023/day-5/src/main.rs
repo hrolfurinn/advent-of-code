11d933ee4e5265ab6d8631cb1d0f83939d181c51
hrolfurinn <106666932+hrolfurinn@users.noreply.github.com>
2023-12-09 14:59:32 -0500

    Day 5 part 1, small bug

--- 2023/day-5/src/main.rs
+++ 2023/day-5/src/main.rs
@@ -1,8 +1,25 @@
 use std::env;
+use std::error::Error;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
+use std::io::{BufRead, BufReader};
 
-fn main() -> Result<()> {
+fn extract_numbers(line: String) -> Vec<i32> {
+    line.split_whitespace()
+        .filter_map(|n| n.parse::<i32>().ok())
+        .collect()
+}
+
+fn map_seed(mut seed: &i32, mut mappings: &Vec<Vec<i32>>) -> i32 {
+    let mut new_seed = *seed;
+    for mapping in mappings.iter() {
+        if mapping[1] <= new_seed && new_seed < mapping[1] + mapping[2] {
+            new_seed = mapping[0] + (new_seed - mapping[1]);
+        }
+    }
+    new_seed
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     env::set_var("RUST_BACKTRACE", "1");
     let input_path = "./input/input.txt";
     let sample_input_path = "./input/sample_input.txt";
@@ -14,20 +31,11 @@ fn main() -> Result<()> {
 
     let mut lines = reader.lines();
 
-    let seeds = if let Some(Ok(line)) = lines.next() {
-        line.split_whitespace()
-            .filter_map(|n| {
-                if let Ok(number) = n.parse::<i32>() {
-                    Some(number)
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<_>>()
-    } else {
-        println!("Failed to read first line.");
-        vec![]
-    };
+    let mut seeds = lines
+        .next()
+        .ok_or("No lines")?
+        .map(extract_numbers)
+        .unwrap();
 
     println!("{:?}", seeds);
 
@@ -36,16 +44,29 @@ fn main() -> Result<()> {
 
     while let Some(Ok(line)) = lines.next() {
         println!("{:?}", line);
-        if line.is_empty() && new_block {
-            // Compute the mappings for all the seeds
-        } else if line.chars().any(|c| c.is_digit(10)) {
-            // Parse numbers, add to mappings variable
-            println!("This is a place where I can find my mappings");
-        } else {
-            new_block = true
+        if line.trim().is_empty() {
+            println!("It's empty");
+            for seed in &mut seeds {
+                *seed = map_seed(seed, &mappings)
+            }
+            mappings.clear();
+            println!("{:?}", seeds);
+            continue;
+        }
+        let numbers = extract_numbers(line);
+        if !numbers.is_empty() {
+            println!("It's numbers");
+            mappings.push(numbers);
         }
     }
-
+    for seed in &mut seeds {
+        *seed = map_seed(seed, &mappings)
+    }
+    mappings.clear();
+    println!("{:?}", seeds);
+    
+    p1 = *seeds.iter().min().unwrap();
+    
     println!("p1: {p1}");
     println!("p2: {p2}");
 
