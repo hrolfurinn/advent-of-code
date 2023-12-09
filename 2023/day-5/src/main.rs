a1b9a3411f7f216e87fcd6aefc4446d2b90e2e12
hrolfurinn <106666932+hrolfurinn@users.noreply.github.com>
2023-12-09 18:28:38 -0500

    Part 1 complete, performance issues for part 2

--- 2023/day-5/src/main.rs
+++ 2023/day-5/src/main.rs
@@ -3,27 +3,28 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn extract_numbers(line: String) -> Vec<i32> {
+fn extract_numbers(line: String) -> Vec<i64> {
     line.split_whitespace()
-        .filter_map(|n| n.parse::<i32>().ok())
+        .filter_map(|n| n.parse::<i64>().ok())
         .collect()
 }
 
-fn map_seed(mut seed: &i32, mut mappings: &Vec<Vec<i32>>) -> i32 {
+fn map_seed(mut seed: &i64, mut mappings: &Vec<Vec<i64>>) -> i64 {
     let mut new_seed = *seed;
     for mapping in mappings.iter() {
         if mapping[1] <= new_seed && new_seed < mapping[1] + mapping[2] {
             new_seed = mapping[0] + (new_seed - mapping[1]);
+            return new_seed;
         }
     }
     new_seed
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    env::set_var("RUST_BACKTRACE", "1");
+    // env::set_var("RUST_BACKTRACE", "1");
     let input_path = "./input/input.txt";
     let sample_input_path = "./input/sample_input.txt";
-    let file = File::open(sample_input_path)?;
+    let file = File::open(input_path)?;
     let reader = BufReader::new(file);
 
     let mut p1 = 0;
@@ -36,37 +37,47 @@ fn main() -> Result<(), Box<dyn Error>> {
         .ok_or("No lines")?
         .map(extract_numbers)
         .unwrap();
+    
+    let mut more_seeds: Vec<i64> = vec![];
 
-    println!("{:?}", seeds);
+    let mut more_seeds = seeds.chunks(2).map(
+        |seed_and_index| {
+            let seed = seed_and_index[0];
+            let count = seed_and_index[1];
+            (seed..(seed + count)).collect::<Vec<_>>()
+        }
+    ).flatten().collect::<Vec<_>>();
 
     let mut new_block = false;
-    let mut mappings: Vec<Vec<i32>> = Vec::new();
+    let mut mappings: Vec<Vec<i64>> = Vec::new();
 
     while let Some(Ok(line)) = lines.next() {
-        println!("{:?}", line);
         if line.trim().is_empty() {
-            println!("It's empty");
             for seed in &mut seeds {
                 *seed = map_seed(seed, &mappings)
             }
+            for seed in &mut more_seeds {
+                *seed = map_seed(seed, &mappings)
+            }
             mappings.clear();
-            println!("{:?}", seeds);
             continue;
         }
         let numbers = extract_numbers(line);
         if !numbers.is_empty() {
-            println!("It's numbers");
             mappings.push(numbers);
         }
     }
     for seed in &mut seeds {
         *seed = map_seed(seed, &mappings)
     }
+    for seed in &mut more_seeds {
+        *seed = map_seed(seed, &mappings)
+    }
     mappings.clear();
-    println!("{:?}", seeds);
-    
+
     p1 = *seeds.iter().min().unwrap();
-    
+    p2 = *more_seeds.iter().min().unwrap();
+
     println!("p1: {p1}");
     println!("p2: {p2}");
 
