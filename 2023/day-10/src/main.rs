d8e329d7cc2447addaff88773e9c098a1f0f1b76
hrolfurinn <106666932+hrolfurinn@users.noreply.github.com>
2023-12-14 14:57:03 -0500

    Added move functionality to map struct

--- 2023/day-10/src/main.rs
+++ 2023/day-10/src/main.rs
@@ -21,36 +21,96 @@ fn load_input(test: bool) -> String {
     }
 }
 
+struct PipeMap {
+    map: Vec<Vec<char>>,
+    start: (usize, usize),
+    location: (usize, usize),
+}
+
+impl PipeMap {
+    pub fn from(input: String) -> Self {
+        let map: Vec<Vec<char>> = input
+            .split_whitespace()
+            .map(|line| line.chars().collect())
+            .collect();
+        let (start_row, start_column) = map
+            .iter()
+            .enumerate()
+            .find_map(|(row_index, row)| {
+                if row.contains(&'S') {
+                    println!("Found S in {:?}", row.iter().collect::<String>());
+                    Some((
+                        row_index,
+                        row.iter()
+                            .enumerate()
+                            .find_map(|(index, c)| if c.eq(&'S') { Some(index) } else { None })
+                            .unwrap(),
+                    ))
+                } else {
+                    None
+                }
+            })
+            .unwrap();
+        println!("Start coords: {:?} and {:?}", start_row, start_column);
+        Self {
+            map: map,
+            start: (start_row, start_column),
+            location: (start_row, start_column),
+        }
+    }
+
+    pub fn go(&mut self, direction: char) {
+        let (mut row, mut column) = self.location;
+        match direction {
+            'n' => row -= 1,
+            'e' => column += 1,
+            'w' => column -= 1,
+            's' => row += 1,
+            _ => unreachable!("Unrecognized direction."),
+        };
+        self.location = (row,column);
+    }
+}
+
+fn to_direction(c: &char) -> (char, char) {
+    match c {
+        '|' => ('n', 's'),
+        '-' => ('e', 'w'),
+        'L' => ('n', 'e'),
+        'J' => ('n', 'w'),
+        '7' => ('s', 'w'),
+        'F' => ('s', 'e'),
+        '.' => ('.', '.'),
+        'S' => ('S', 'S'),
+        _ => unreachable!("Found invalid character"),
+    }
+}
+
+fn opposite_direction(c: &char) -> char {
+    match c {
+        'n' => 's',
+        'e' => 'w',
+        'w' => 'e',
+        's' => 'n',
+        _ => unreachable!("Unrecognized direction."),
+    }
+}
+
 fn main() -> Result<()> {
     env::set_var("RUST_BACKTRACE", "1");
     let test = true;
 
     let input = load_input(test);
 
-    let map: Vec<Vec<char>> = input
-        .split_whitespace()
-        .map(|line| line.chars().collect())
-        .collect();
-    let [row, column] = map
-        .iter()
-        .enumerate()
-        .find_map(|(row_index, row)| {
-            if row.contains(&'S') {
-                println!("Found S in {:?}", row.iter().collect::<String>());
-                Some([
-                    row_index,
-                    row.iter()
-                        .enumerate()
-                        .find_map(|(index, c)| if c.eq(&'S') { Some(index) } else { None })
-                        .unwrap(),
-                ])
-            } else {
-                None
-            }
-        })
-        .unwrap();
+    let mut map = PipeMap::from(input);
+    println!("Start: {:?}", map.start);
+    map.go('s');
+    println!("Current: {:?}", map.location);
 
-    println!("Coords: {:?} and {:?}", row, column);
+    let directions = ['n', 'e', 's', 'w'];
+    let path: Vec<(usize, usize)> = Vec::new();
+
+    for direction in directions {}
 
     let mut p1: i64 = 0;
     let mut p2: i64 = 0;
