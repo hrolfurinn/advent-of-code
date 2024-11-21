62a530f27ca794a7fb4fc6019eb1989fa2232382
Hrólfur E <106666932+hrolfurinn@users.noreply.github.com>
2024-11-21 02:42:26 -0500

    day 5 p 2 still buggy

--- 2015/day-5/src/main.rs
+++ 2015/day-5/src/main.rs
@@ -1,31 +1,49 @@
+use std::collections::HashSet;
 use std::fs::read_to_string;
 use std::io::Result;
-use std::collections::HashSet;
 
 fn process_line(word: &str) -> u32 {
+    println!("--------------------------");
+    println!("inspecting {word}");
     let mut twin_pairs = HashSet::new();
+
+    let length = word.len();
 
     let mut has_dupe_twin_pair = false;
     let mut has_exe = false;
 
-    for char_triple in word.as_bytes().windows(3) {
+    for (ix, char_triple) in word.as_bytes().windows(3).enumerate() {
         let c1 = char_triple[0];
         let c2 = char_triple[1];
         let c3 = char_triple[2];
-        match (c1 == c2, c1 == c3) {
-            (_, true) => has_exe = true,
-            (true,false) => { 
-                if twin_pairs.contains(&c1) { 
-                    has_dupe_twin_pair = true 
-                } else { twin_pairs.insert(c1); };
-            },
-            _ => {}
+        println!("inspecting {},{},{}", c1, c2, c3);
+
+        has_dupe_twin_pair |= twin_pairs.contains(&(c1, c2));
+
+        if ix == length - 3 {
+            println!("End of the word");
+            has_dupe_twin_pair |= twin_pairs.contains(&(c2, c3));
         }
+
+        if c1 == c3 {
+            println!("First and last identical");
+            has_exe = true;
+
+            if c1 == c2 {
+                println!("So is the second, holding off on adding it to HashSet");
+                continue;
+            };
+        }
+        println!("Adding first two to HashSet");
+        twin_pairs.insert((c1, c2));
     }
 
-    if has_exe && has_dupe_twin_pair { return 1 }
+    if has_exe && has_dupe_twin_pair {
+        println!("SUCCESS");
+        return 1;
+    }
+    println!("FAILURE");
     0
-
 }
 
 fn main() -> Result<()> {
