17230d10a7c84c7c48978235523c0d7c9d2cce05
Hrólfur E <106666932+hrolfurinn@users.noreply.github.com>
2024-11-30 23:58:17 -0500

    gitignore

---
+++ 2024/day-1/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs::read_to_string;
+use std::io::{Read, Result};
+
+fn main() -> Result<()> {
+    let test = false;
+
+    let input = load_input(test);
+
+    Ok(())
+}
+
+fn load_input(test: bool) -> String {
+    let path = if let Some(arg) = std::env::args().nth(1) {
+        if arg == "--default-input" {
+            if test {
+                "./input/sample_input.txt"
+            } else {
+                "./input/input.txt"
+            }.to_string()
+        } else {
+            String::new()
+        }
+    } else {
+        String::new()
+    };
+    if path != "" {
+        read_to_string(&path).unwrap_or_else(|e| {
+            eprintln!("Failed to read input file: {e}");
+            std::process::exit(1);
+        })
+    } else {
+        let mut buffer = String::new();
+        std::io::stdin().read_to_string(&mut buffer).unwrap();
+        buffer
+    }
+}
