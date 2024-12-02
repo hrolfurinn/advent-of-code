0a427fa8052e06afa15a5113b7f0c36edc89037b
Hrólfur E <106666932+hrolfurinn@users.noreply.github.com>
2024-12-02 00:41:59 -0500

    2024 day 2 p 1 & 2 horrible

--- 2024/day-2/src/main.rs
+++ 2024/day-2/src/main.rs
@@ -3,6 +3,40 @@ use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::io::{Read, Result};
 
+fn check_safety(parts: Vec<i32>) -> (usize, usize) {
+    let mut parts = parts.iter();
+    let mut curr = parts.next().unwrap();
+    let mut up_down = 0;
+    for (ix, val) in parts.enumerate() {
+        if (val - curr).abs() > 3 {
+            return (0, ix);
+        };
+        if (val - curr).abs() < 1 {
+            return (0, ix);
+        };
+        if up_down == 0 {
+            up_down = match val > curr {
+                true => 1,
+                false => -1,
+                _ => unreachable!("bno"),
+            };
+            curr = val;
+            continue;
+        }
+        if val > curr {
+            if up_down != 1 {
+                return (0, ix);
+            }
+        } else {
+            if up_down != -1 {
+                return (0, ix);
+            }
+        }
+        curr = val;
+    }
+    return (1, 0);
+}
+
 fn main() -> Result<()> {
     let test = false;
 
@@ -12,7 +46,22 @@ fn main() -> Result<()> {
     let mut p2 = 0;
 
     for line in input.lines() {
-        let parts = line.split_whitespace().map(|s| s.trim()).collect::<Vec<_>>();
+        let mut parts = line
+            .split_whitespace()
+            .map(|s| s.trim().parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        let (is_safe, _) = check_safety(parts.clone());
+        p1 += is_safe;
+        if is_safe == 1 {
+            p2 += 1;
+            continue;
+        };
+        for ix in 0..parts.len() {
+            let mut parts_clone = parts.clone();
+            parts_clone.remove(ix);
+            if check_safety(parts_clone).0 == 1 { p2 += 1; break }
+        }
     }
 
     println!("p1: {p1}\np2: {p2}");
@@ -27,7 +76,8 @@ fn load_input(test: bool) -> String {
                 "./input/sample_input.txt"
             } else {
                 "./input/input.txt"
-            }.to_string()
+            }
+            .to_string()
         } else {
             String::new()
         }
