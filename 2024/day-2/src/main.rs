461636d76ae84e3ef5f3c3b765577bf7c0be6381
Hrólfur E <106666932+hrolfurinn@users.noreply.github.com>
2024-12-02 16:37:26 -0500

    2024 day 2 p 2 linear time

--- 2024/day-2/src/main.rs
+++ 2024/day-2/src/main.rs
@@ -1,40 +1,42 @@
 use itertools::Itertools;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 use std::io::{Read, Result};
 
-fn check_safety(parts: Vec<i32>) -> (usize, usize) {
-    let mut parts = parts.iter();
-    let mut curr = parts.next().unwrap();
-    let mut up_down = 0;
-    for (ix, val) in parts.enumerate() {
-        if (val - curr).abs() > 3 {
-            return (0, ix);
-        };
-        if (val - curr).abs() < 1 {
-            return (0, ix);
-        };
-        if up_down == 0 {
-            up_down = match val > curr {
-                true => 1,
-                false => -1,
-                _ => unreachable!("bno"),
-            };
-            curr = val;
+fn check_safety(
+    dist_set: &HashSet<i32>,
+    ok_pos_dists: &HashSet<i32>,
+    ok_neg_dists: &HashSet<i32>,
+) -> bool {
+    dist_set.is_subset(ok_pos_dists) || dist_set.is_subset(ok_neg_dists)
+}
+
+fn check_substrings(dists: &[i32], ok_dists: &HashSet<i32>) -> bool {
+    let mut dists = dists.iter();
+    let first = dists.next().unwrap();
+    if !ok_dists.contains(first) {
+        let next = dists.next().unwrap();
+        let mut ok = dists
+            .map(|&v| v)
+            .collect::<HashSet<_>>()
+            .is_subset(ok_dists);
+        ok &= ok_dists.contains(next) || ok_dists.contains(&(next + first));
+        return ok;
+    }
+    while let Some(curr) = dists.next() {
+        if ok_dists.contains(curr) {
             continue;
         }
-        if val > curr {
-            if up_down != 1 {
-                return (0, ix);
-            }
-        } else {
-            if up_down != -1 {
-                return (0, ix);
-            }
+        if let Some(next) = dists.next() {
+            let mut ok = ok_dists.contains(&(next + curr));
+            ok &= dists
+                .map(|&v| v)
+                .collect::<HashSet<_>>()
+                .is_subset(ok_dists);
+            return ok;
         }
-        curr = val;
     }
-    return (1, 0);
+    return true;
 }
 
 fn main() -> Result<()> {
@@ -45,23 +47,30 @@ fn main() -> Result<()> {
     let mut p1 = 0;
     let mut p2 = 0;
 
+    let ok_pos_dists = HashSet::from([1, 2, 3]);
+    let ok_neg_dists = HashSet::from([-1, -2, -3]);
+
     for line in input.lines() {
-        let mut parts = line
+        let parts = line
             .split_whitespace()
             .map(|s| s.trim().parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
-
-        let (is_safe, _) = check_safety(parts.clone());
-        p1 += is_safe;
-        if is_safe == 1 {
+            .collect::<Vec<_>>();
+        let dists = parts.iter().tuple_windows().map(|(a, b)| b - a);
+        let dist_set = dists.clone().collect::<HashSet<_>>();
+        if check_safety(&dist_set, &ok_pos_dists, &ok_neg_dists) {
+            p1 += 1;
             p2 += 1;
             continue;
-        };
-        for ix in 0..parts.len() {
-            let mut parts_clone = parts.clone();
-            parts_clone.remove(ix);
-            if check_safety(parts_clone).0 == 1 { p2 += 1; break }
         }
+        let sign = dists.clone().filter(|&v| v > 0).collect::<Vec<_>>().len() > dists.len() - 2;
+        let ok_dists;
+        if sign {
+            ok_dists = &ok_pos_dists;
+        } else {
+            ok_dists = &ok_neg_dists;
+        }
+        let new_method = check_substrings(&dists.collect::<Vec<_>>(), &ok_dists);
+        p2 += new_method as i32;
     }
 
     println!("p1: {p1}\np2: {p2}");
