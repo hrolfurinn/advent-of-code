2408efa56c2ce6e3cac31abad7d06f721545d044
Hrólfur Eyjólfsson <ralo@velin.mynetworksettings.com>
2024-12-06 02:19:34 -0500

    Phantom method, abandoned due to example Option 4

--- 2024/day-6/src/main.rs
+++ 2024/day-6/src/main.rs
@@ -11,6 +11,7 @@ use std::io::{Read, Result};
 // instances where the guard crosses previous path sections which couldn't have a new obstacle
 // placed.
 
+#[derive(Clone, Copy, PartialEq)]
 enum Direction {
     Up,
     Down,
@@ -37,7 +38,7 @@ impl Direction {
     }
 }
 
-#[derive(Clone,Copy)]
+#[derive(Clone, Copy)]
 struct Point {
     x: usize,
     y: usize,
@@ -46,10 +47,22 @@ struct Point {
 impl Point {
     fn add(&self, direction: &Direction) -> Self {
         match direction {
-            Direction::Down => Point { x: self.x, y: self.y + 1 },
-            Direction::Up => Point { x: self.x, y: self.y - 1 },
-            Direction::Right => Point { x: self.x + 1, y: self.y },
-            Direction::Left => Point { x: self.x - 1, y: self.y },
+            Direction::Down => Point {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Direction::Up => Point {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Direction::Right => Point {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Direction::Left => Point {
+                x: self.x - 1,
+                y: self.y,
+            },
         }
     }
 }
@@ -59,8 +72,8 @@ struct Grid {
     width: usize,
     height: usize,
     obstacles: Vec<Vec<bool>>,
-    visited_points: Vec<Vec<bool>>,
-    phantom_visited_points: Vec<Vec<bool>>,
+    visited_points: Vec<Vec<Option<Direction>>>,
+    phantom_visited_points: Vec<Vec<Option<Direction>>>,
     guard: Point,
     original_guard: Point,
     guard_direction: Direction,
@@ -72,65 +85,86 @@ impl Grid {
         for (y, line) in input.lines().enumerate() {
             if self.obstacles.is_empty() {
                 self.width = line.len();
-                self.obstacles.push(vec![false;self.width + 2]);
+                self.obstacles.push(vec![false; self.width + 2]);
             }
             if let Some(x) = line.find("^") {
                 self.guard = Point { x: x + 1, y: y + 1 };
                 self.original_guard = Point { x: x + 1, y: y + 1 };
             }
-            let mut row = vec![false;1];
+            let mut row = vec![false; 1];
             row.extend(line.trim().bytes().map(|byte| byte == b'#').collect_vec());
             row.push(false);
             self.obstacles.push(row);
         }
-        self.obstacles.push(vec![false;self.width + 2]);
+        self.obstacles.push(vec![false; self.width + 2]);
         self.height = self.obstacles.len();
-        self.visited_points = vec![vec![false; self.width + 2]; self.height + 2];
-        self.phantom_visited_points = vec![vec![false; self.width + 2]; self.height + 2];
+        self.visited_points = vec![vec![None; self.width + 2]; self.height + 2];
+        self.phantom_visited_points = vec![vec![None; self.width + 2]; self.height + 2];
         self.new_obstacles = vec![vec![false; self.width + 2]; self.height + 2];
-        self.visit(self.guard);
+        self.visit(self.guard, self.guard_direction);
     }
 
-    fn visit(&mut self, point: Point) {
-        self.visited_points[point.y][point.x] = true;
+    fn visit(&mut self, point: Point, direction: Direction) {
+        self.visited_points[point.y][point.x] = Some(direction);
     }
 
-    fn phantom_visit(&mut self, point: Point) {
-        self.phantom_visited_points[point.y][point.x] = true;
+    fn phantom_visit(&mut self, point: Point, direction: Direction) {
+        self.phantom_visited_points[point.y][point.x] = Some(direction);
     }
 
-    fn is_visited(&self, point: Point) -> bool {
-        self.visited_points[point.y][point.x] || self.phantom_visited_points[point.y][point.x]
+    fn add_obstacle(&mut self, point: Point) {
+        let potential_obstacle = point.add(&self.guard_direction);
+        if self.is_off_grid(potential_obstacle) {
+            return;
+        }
+        if let Some(visted_direction) = self.visited_points[point.y][point.x] {
+            if visted_direction == self.guard_direction.turn_right() {
+                self.new_obstacles[potential_obstacle.y][potential_obstacle.x] = true;
+                return;
+            }
+        }
+        if let Some(visted_direction) = self.phantom_visited_points[point.y][point.x] {
+            if visted_direction == self.guard_direction.turn_left() {
+                self.new_obstacles[potential_obstacle.y][potential_obstacle.x] = true;
+                return;
+            }
+        }
     }
 
     fn has_obstacle(&self, point: &Point) -> bool {
         self.obstacles[point.y][point.x]
     }
 
+    fn is_off_grid(&self, point: Point) -> bool {
+        let banned_coords = [0, self.width + 1, self.height + 1];
+        banned_coords.contains(&point.x) || banned_coords.contains(&point.y)
+    }
+
     fn guard_step(&mut self) -> bool {
         let new_point = self.guard.add(&self.guard_direction);
-        let banned_coords = [0,self.width + 1, self.height + 1];
-        if banned_coords.contains(&new_point.x) || banned_coords.contains(&new_point.y) { return false }
-        if !self.has_obstacle(&new_point) { self.move_guard(new_point); return true }
+        if self.is_off_grid(new_point) {
+            return false;
+        }
+        if !self.has_obstacle(&new_point) {
+            self.add_obstacle(new_point);
+            self.guard = new_point;
+            return true;
+        }
         self.guard_direction = self.guard_direction.turn_right();
         self.guard_step()
     }
-    
+
     fn phantom_guard_step(&mut self) -> bool {
         let new_point = self.guard.add(&self.guard_direction);
-        let banned_coords = [0,self.width + 1, self.height + 1];
-        if banned_coords.contains(&new_point.x) || banned_coords.contains(&new_point.y) { return false }
-        if !self.has_obstacle(&new_point) { 
+        if self.is_off_grid(new_point) {
+            return false;
+        }
+        if !self.has_obstacle(&new_point) {
             self.guard = new_point;
-            return true 
+            return true;
         }
         self.guard_direction = self.guard_direction.turn_left();
         self.guard_step()
-    }
-
-    fn move_guard(&mut self, point: Point) {
-        if self.is_visited(point) { self.new_obstacles[point.y][point.x] = true; }
-        self.guard = point;
     }
 }
 
@@ -154,19 +188,40 @@ fn main() -> Result<()> {
         new_obstacles: Vec::new(),
     };
 
+
     grid.from(&input);
 
+    println!("Guard is at {} {}", grid.guard.x, grid.guard.y);
     grid.guard_direction = Direction::Down;
 
-    while grid.phantom_guard_step() { grid.phantom_visit(grid.guard); }
+    while grid.phantom_guard_step() {
+        grid.phantom_visit(grid.guard, grid.guard_direction);
+    }
 
     grid.guard = grid.original_guard;
-    grid.guard_direction = Direction::Down;
+    grid.guard_direction = Direction::Up;
 
-    while grid.guard_step() { grid.visit(grid.guard); }
+    while grid.guard_step() {
+        grid.visit(grid.guard, grid.guard_direction);
+    }
 
-    p1 += grid.visited_points.iter().map(|v| v.iter().map(|&b| if b { 1 } else { 0 }).sum::<usize>()).sum::<usize>();
-    p2 += grid.new_obstacles.iter().map(|v| v.iter().map(|&b| if b { 1 } else { 0 }).sum::<usize>()).sum::<usize>();
+    p1 += grid
+        .visited_points
+        .iter()
+        .map(|v| v.iter().map(|&d| if d.is_some() { 1 } else { 0 }).sum::<usize>())
+        .sum::<usize>();
+    for (y, row) in grid.new_obstacles.iter().enumerate() {
+        for (x, &d) in row.iter().enumerate() {
+            if d {
+            println!("{x} {y}");
+            }
+        }
+    }
+    p2 += grid
+        .new_obstacles
+        .iter()
+        .map(|v| v.iter().map(|&b| if b { 1 } else { 0 }).sum::<usize>())
+        .sum::<usize>();
 
     println!("p1: {p1}\np2: {p2}");
 
