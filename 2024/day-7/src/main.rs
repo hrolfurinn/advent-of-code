b074e268061366d485782188860b352a5f723f5c
Hrólfur Eyjólfsson <ralo@velin.mynetworksettings.com>
2024-12-07 04:20:27 -0500

    Hiding commit msg in subtitle so Logi doesn't see
    Recursive instead of iterating through subset, with filtering

--- 2024/day-7/src/main.rs
+++ 2024/day-7/src/main.rs
@@ -3,18 +3,6 @@ use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 use std::io::{Read, Result};
 
-fn powerset_3(s: Vec<u32>) -> Vec<Vec<u32>> {
-    (0..3usize.pow(s.len() as u32)).map(|i| {
-        s.iter().enumerate().map(|(t, _)| ((i as u32) / 3_u32.pow(t as u32)) % 3_u32).collect_vec()
-     }).collect()
-}   
-
-fn powerset_2(s: Vec<u32>) -> Vec<Vec<u32>> {
-    (0..2usize.pow(s.len() as u32)).map(|i| {
-        s.iter().enumerate().map(|(t, _)| ((i as u32) / 2_u32.pow(t as u32)) % 2_u32).collect_vec()
-     }).collect()
-}   
-
 fn main() -> Result<()> {
     let test = false;
 
@@ -28,37 +16,25 @@ fn main() -> Result<()> {
         let numbers = numbers_str.trim().split(" ").map(|num| num.parse::<u64>().unwrap()).collect::<Vec<u64>>();
         let target = target_str.trim().parse::<u64>().unwrap();
 
-        let mut numbers_iter = numbers.iter();
+        let mut numbers_iter = numbers.clone().into_iter();
         let first_num = numbers_iter.next().unwrap();
 
-        let subsets = powerset_2((0..numbers.len() - 1).map(|num| num as u32).collect_vec());
-        for subset in subsets {
-            let new_first_num = first_num.clone();
-            let value_candidate = numbers_iter.clone().enumerate().fold(new_first_num, |acc, (ix, num)| {
-                if subset[ix] == 1 {
-                    return acc * num;
-                } else {
-                    return acc + num;
-                }
-            });
-            if value_candidate == target { p1 += target; break;}
-        }
-        let subsets = powerset_3((0..numbers.len() - 1).map(|num| num as u32).collect_vec());
-        for subset in subsets {
-            let new_first_num = first_num.clone();
-            let value_candidate = numbers_iter.clone().enumerate().fold(new_first_num, |acc, (ix, num)| {
-                if subset[ix] == 2 {
-                    acc * 10_u64.pow(num.ilog10() + 1) + num
-                }else if subset[ix] == 1{
-                    return acc * num;
-                } else {
-                    return acc + num;
-                }
-            });
-            if value_candidate == target { p2 += target; break;}
-        }
-            
+        if numbers_iter.clone().fold(vec![first_num], |acc_vec, num| {
+            acc_vec.into_iter().flat_map(|acc| {
+                let add = acc + num;
+                let mult = acc * num;
+                vec![add,mult]
+            }).filter(|val| *val <= target).collect()
+        }).contains(&target) {p1 += target}
 
+        if numbers_iter.fold(vec![first_num], |acc_vec, num| {
+            acc_vec.into_iter().flat_map(|acc| {
+                let add = acc + num;
+                let concat = acc * 10_u64.pow(num.ilog10() + 1) + num;
+                let mult = acc * num;
+                vec![add,concat,mult]
+            }).filter(|val| *val <= target).collect()
+        }).contains(&target) {p2 += target}
         
     }
 
