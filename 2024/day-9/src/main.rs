951795f4630dc784e287894d60f16a2fe5857363
hrolfurinn <hrolfurinn@gmail.com>
2024-12-09 07:29:51 -0500

    2024 day 9 p1 & p2 cleaned

--- 2024/day-9/src/main.rs
+++ 2024/day-9/src/main.rs
@@ -1,64 +1,27 @@
 use itertools::Itertools;
-use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 use std::io::{Read, Result};
-use std::ops::{Index, IndexMut};
-
-fn print_flat_vec(flat_vec: &Vec<Option<usize>>) {
-    for char in flat_vec {
-        print!(
-            "{}",
-            if char.is_some() {
-                char.unwrap().to_string()
-            } else {
-                ".".to_string()
-            }
-        )
-    }
-    println!();
-}
-
-fn print_blocks(blocks: &Vec<(bool, i32, Option<usize>, bool)>) {
-    for block in blocks.iter() {
-        print!("|");
-        (0..block.1).for_each(|_| {
-            print!(
-                "{}",
-                if block.0 {
-                    ".".to_string()
-                } else {
-                    block.2.unwrap().to_string()
-                }
-            )
-        });
-    }
-    println!();
-}
 
 fn collapse_flat_vec(flat_vec: &Vec<Option<usize>>) -> usize {
     let mut output = flat_vec.clone();
     let mut ix = 0;
     while ix < output.len() {
-        print_flat_vec(&output);
         if output[ix].is_some() {
             ix += 1;
             continue;
         }
-        while ix < output.len() {
-            if let Some(last) = output.pop() {
-                if last.is_none() {
-                    continue;
-                }
-                output[ix] = last;
-                break;
+        if let Some(last) = output.pop() {
+            if last.is_none() {
+                continue;
             }
+            output[ix] = last;
+            ix += 1;
         }
-        ix += 1;
     }
     output
         .iter()
         .enumerate()
-        .map(|(ix, n)| { println!("{} * {} = {}", ix, n.unwrap(), ix * n.unwrap()); ix * n.unwrap() } )
+        .map(|(ix, n)| ix * n.unwrap_or(0))
         .sum()
 }
 
@@ -70,7 +33,14 @@ fn main() -> Result<()> {
     let mut p1 = 0;
     let mut p2 = 0;
 
-    let mut blocks: Vec<(bool, i32, Option<usize>, bool)> = Vec::new(); // (is_free,space,file_id,has_been_moved)
+    // We store the data efficiently, implementing a description of the file location and contents
+    // instead of a data structure containing all blocks in the system and their associated values
+    // in objects. The descriptions are enough for a successful logic built on top of them.
+    //
+    // Each block in the file system has these associated data:
+    // (is_free,space,file_id,has_been_moved)
+    // Note: Technically, each "block" here is "space" many blocks, based on the problem statement.
+    let mut blocks: Vec<(bool, i32, Option<usize>, bool)> = Vec::new(); 
 
     for (ix, space) in input
         .trim()
@@ -98,102 +68,94 @@ fn main() -> Result<()> {
 
     p1 += collapse_flat_vec(&flat_vec);
 
-    let mut ix = blocks.len() as i32 - 1;
-    while ix >= 0 {
-        println!("In {ix}");
-        print_blocks(&blocks);
-
-        let (is_free, space, _, has_been_moved) = blocks[ix as usize];
+    // TODO: I wish I didn't have to use a for loop here but having the blocks object in the while
+    // statement itself was causing me borrowing issues, presumably because the while loop didn't
+    // like its children taking ownership of the blocks if it itself was using the object.
+    for ix in (0..blocks.len() as i32).rev() {
+        let (is_free, needed_space, _, has_been_moved) = blocks[ix as usize];
         if is_free || has_been_moved {
-            ix -= 1;
             continue;
         }
-        println!("Need space: {space}");
-        if let Some(block_ix) =
+
+        // We look for a block earlier in the file system that is free and has enough space. We
+        // then free up the block associate with our file, making sure to merge adjacent blocks,
+        // and replace the contents of the free block with the file, leaving leftover space free.
+        if let Some(free_block_ix) =
             blocks
                 .iter()
                 .enumerate()
                 .position(|(block_ix, (block_is_free, block_space, _, _))| {
-                    block_ix < ix as usize && *block_is_free && space.le(block_space)
+                    block_ix < ix as usize && *block_is_free && needed_space.le(block_space)
                 })
         {
-            println!("Found needed space at {block_ix}");
-
             // We start by removing the to-be-moved value from the file system. Since this will be
             // replaced by free space, we need to take care to remove its neighbors as well, which
             // we do safely without checking whether one of the neighbors is the to-be-moved-to
-            // block in the file system.
-            let mut right_side = None; // We are not guaranteed a right side in bounds
-            if ix + 1 < blocks.len() as i32 {
-                right_side = Some(blocks.remove(ix as usize + 1));
-            }
-            let (_, space, file_id, _) = blocks.remove(ix as usize);
-            let left_side = blocks.remove(ix as usize - 1); // We are guaranteed a left side in bounds
+            // block in the file system. We are just freeing up the space around it and the index
+            // of our free block will remain the same, and not affect the logic later.
 
-            println!("Removed all three surrounding elements");
-            print_blocks(&blocks);
+            // We will insert and remove (ix-1,ix,ix+1) values all at the same index which simplifies
+            // the logic, but then rightmost value needs to be removed last and added first in order
+            // for the original order to be preserved.
+            let insertion_index = ix as usize - 1;
+
+            // Note that this working correctly assumes and ensures that there are no more than one
+            // free space blocks in a row anywhere, so we don't need to look further than the
+            // immediate neighbors.
+            let left_side = blocks.remove(insertion_index); // We are guaranteed a left side in bounds
+            let (_, needed_space, file_id, _) = blocks.remove(insertion_index);
+            let mut right_side = None; // We are not guaranteed a right side in bounds
+            if insertion_index < blocks.len() {
+                right_side = Some(blocks.remove(insertion_index));
+            }
+
             if let Some(right_side) = right_side {
                 if right_side.0 {
+                    // If the right side is free, we add free space for both the removed target
+                    // file and the right side itself.
                     blocks.insert(
-                        ix as usize - 1,
-                        (true, space + right_side.1, None, false),
+                        insertion_index,
+                        (true, needed_space + right_side.1, None, false),
                     );
                 } else {
-                    blocks.insert(ix as usize - 1, right_side);
-                    blocks.insert(ix as usize - 1, (true, space, None, false));
+                    // Otherwise, we place it as it was, along with a freed block where the file was
+                    blocks.insert(insertion_index, right_side);
+                    blocks.insert(insertion_index, (true, needed_space, None, false));
                 }
             } else {
-                blocks.insert(ix as usize - 1, (true, space, None, false));
+                blocks.insert(insertion_index, (true, needed_space, None, false));
             }
             if left_side.0 {
-                let (_, curr_space, _, _) = blocks.remove(ix as usize - 1);
+                let (_, curr_space, _, _) = blocks.remove(insertion_index);
                 blocks.insert(
-                    ix as usize - 1,
+                    insertion_index,
                     (true, curr_space + left_side.1, None, false),
                 );
             } else {
-                blocks.insert(ix as usize - 1, left_side);
-            }
-            println!("Attempted merging");
-            print_blocks(&blocks);
-
-            // Removing the block of free space from the filesystem leaves all indices after it
-            // shifted by 1, which includes all relevant indices in the rest of this loop
-            let (block_is_free, block_space, _, _) = blocks.remove(block_ix);
-            if !block_is_free {
-                panic!("Found non-free block")
-            };
-            if ix == 8855 && block_ix == 8854 {
-                println!("print here");
-                println!(
-                    "if statement {} {}",
-                    ix as usize - 1 + 1 < blocks.len(),
-                    blocks[ix as usize - 1 + 1].0
-                )
+                blocks.insert(insertion_index, left_side);
             }
 
-            // Move block in file system
-            if block_space > space {
-                let prev_len = blocks.len();
-                blocks.insert(block_ix, (true, block_space - space, None, false));
-                if prev_len == blocks.len() {
-                    panic!("Did not change length");
-                }
+            let (_, free_block_space, _, _) = blocks.remove(free_block_ix);
+
+            // We can now move the file content to the free space
+            if free_block_space > needed_space {
+                // Start by adding the rest of the free space to its location again
+                blocks.insert(
+                    free_block_ix,
+                    (true, free_block_space - needed_space, None, false),
+                );
             }
-            blocks.insert(block_ix, (false, space, file_id, true));
+            // Then we can safely add the file IDs to the free space location without checks
+            blocks.insert(free_block_ix, (false, needed_space, file_id, true));
         }
-        ix -= 1;
-    }
-    let mut flat_vec = Vec::new();
-    for (_, space, file_id, _) in blocks {
-        flat_vec.extend(vec![file_id; space as usize]);
     }
 
-    println!("part 2:");
-    print_flat_vec(&flat_vec);
-    println!("part 2 collapsing:");
-
-    p2 += flat_vec.iter().enumerate().map(|(ix,n)| ix * n.unwrap_or(0)).sum::<usize>();
+    p2 += blocks
+        .iter()
+        .flat_map(|&(_, space, file_id, _)| vec![file_id; space as usize])
+        .enumerate()
+        .map(|(ix, n)| ix * n.unwrap_or(0))
+        .sum::<usize>();
 
     println!("p1: {p1}\np2: {p2}");
 
