3a604415d32bc552f81ba781002bde673ba54e8a
hrolfurinn <hrolfurinn@gmail.com>
2024-12-11 14:13:24 -0500

    2024 day 11 p2

--- 2024/day-11/src/main.rs
+++ 2024/day-11/src/main.rs
@@ -4,6 +4,43 @@ use std::fs::read_to_string;
 use std::io::{Read, Result};
 use std::ops::{Index, IndexMut};
 
+fn get_digit_num(stone: u64) -> u32 {
+    if stone < 10 {
+        return 1;
+    }
+    get_digit_num(stone / 10) + 1
+}
+
+fn count_final_stones(stone: u64, iterations: u32, cache: &mut HashMap<(u64,u32),u64>) -> u64 {
+    if iterations == 0 {
+        return 1;
+    }
+    if let Some(result) = cache.get(&(stone,iterations)) {
+        return *result
+    }
+    if stone == 0 {
+        let result = count_final_stones(1, iterations - 1, cache);
+        cache.insert((stone,iterations),result);
+        return result
+    }
+    let num_digits = get_digit_num(stone);
+    if num_digits % 2 == 0 {
+        let half = 10_u64.pow(num_digits / 2);
+        vec![stone / half, stone % half];
+        // We compute the second half before the first since it is smaller and therefore more
+        // likely to affect the cache
+        let second_result = count_final_stones(stone % half, iterations - 1, cache);
+        let first_result = count_final_stones(stone / half, iterations - 1, cache);
+        let result = first_result + second_result;
+        cache.insert((stone,iterations),result);
+        return result
+    } else {
+        let result = count_final_stones(stone * 2024, iterations - 1, cache);
+        cache.insert((stone,iterations),result);
+        return result
+    }
+}
+
 fn main() -> Result<()> {
     let test = false;
 
@@ -18,25 +55,16 @@ fn main() -> Result<()> {
         .map(|str| str.parse::<u64>().unwrap())
         .collect_vec();
 
-    for _ in 0..25 {
-        stones = stones
-            .into_iter()
-            .flat_map(|stone| {
-                if stone == 0 {
-                    return vec![1];
-                }
-                let num_digits = stone.ilog10() + 1;
-                if num_digits % 2 == 0 {
-                    let half = 10_u64.pow(num_digits / 2);
-                    vec![stone / half, stone % half]
-                } else {
-                    vec![stone * 2024]
-                }
-            })
-            .collect_vec();
+    let mut cache = HashMap::new();
+
+    for stone in stones.iter() {
+        p1 += count_final_stones(*stone, 25, &mut cache);
     }
 
-    p1 += stones.len();
+    for stone in stones.iter() {
+        p2 += count_final_stones(*stone, 75, &mut cache);
+    }
+
 
     println!("p1: {p1}\np2: {p2}");
 
