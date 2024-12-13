bdc2a8fa741bb2365e7ad951ec0ba36e2cc8ce82
hrolfurinn <hrolfurinn@gmail.com>
2024-12-13 19:44:18 +0900

    Assume linear indpendence

--- 2024/day-13/src/main.rs
+++ 2024/day-13/src/main.rs
@@ -19,42 +19,19 @@ fn solve_linear_eq(a: Point, b: Point, x: Point) -> (i64, i64) {
     let mut n = 0;
     let mut m = 0;
 
-    // First we deal with the case where the vectors are linearly dependent
-    if a.1 * b.0 == a.0 * b.1 {
-        // Checks x/y ratio, i.e. if the angle is the same
-        // If X is not of the same angle, it's unreachable and we continue
-        if a.1 * x.0 == a.0 * x.1 {
-            // We only care about one coordinate, since the angles are all the same
-            let smaller = a.0.min(b.0);
-            let larger = a.0.max(b.0);
-            let gcd = get_gcd(smaller, larger);
-            let needed_parts = x.0 / gcd;
-            let num_larger = gcd * needed_parts / larger;
-            if (x.0 % (num_larger * larger) % smaller) == 0 {
-                if larger == a.0 {
-                    n = num_larger;
-                    m = (x.0 % (num_larger * larger)) / smaller;
-                } else {
-                    n = (x.0 % (num_larger * larger)) / smaller;
-                    m = num_larger;
-                }
-            }
-        }
-    } else {
-        // We can apply the solution
-        // N = D^{-1} X
-        // i.e.
-        // N = (1/det(D)) * adj(D) * X
-        // n = (1/det(D)) ( x_1*b_2 - x_2*b_1 )
-        // m = (1/det(D)) ( x_2*a_1 - x_1*a_1 )
-        let det = (a.0 * b.1) - (a.1 * b.0);
-        let adj_x = ((x.0 * b.1) - (x.1 * b.0), (x.1 * a.0) - (x.0 * a.1));
-        if adj_x.0 % det == 0 && adj_x.1 % det == 0 {
-            n = adj_x.0 / det;
-            m = adj_x.1 / det;
-        }
+    // We can apply the solution
+    // N = D^{-1} X
+    // i.e.
+    // N = (1/det(D)) * adj(D) * X
+    // n = (1/det(D)) ( x_1*b_2 - x_2*b_1 )
+    // m = (1/det(D)) ( x_2*a_1 - x_1*a_1 )
+    let det = (a.0 * b.1) - (a.1 * b.0);
+    let adj_x = ((x.0 * b.1) - (x.1 * b.0), (x.1 * a.0) - (x.0 * a.1));
+    if adj_x.0 % det == 0 && adj_x.1 % det == 0 {
+        n = adj_x.0 / det;
+        m = adj_x.1 / det;
     }
-    (n,m)
+    (n, m)
 }
 
 fn main() -> Result<()> {
@@ -87,17 +64,16 @@ fn main() -> Result<()> {
         // Where D is the direction matrix (A B), i.e. with columns as our possible directions,
         // and N = (n // m) is a column vector with rows n and m.
         //
-        // We have a closed form solution when the A and B are independent.
+        // We have a closed form solution since all A,B are linearly indpendent in input.
 
-        let (n,m) = solve_linear_eq(a,b,x);
-        p1 += n*3 + m;
+        let (n, m) = solve_linear_eq(a, b, x);
+        p1 += n * 3 + m;
         // Part 2 works the same but we change x
 
         let x = (x.0 + 10000000000000, x.1 + 10000000000000);
 
-        let (n,m) = solve_linear_eq(a,b,x);
-        p2 += n*3 + m;
-
+        let (n, m) = solve_linear_eq(a, b, x);
+        p2 += n * 3 + m;
     }
 
     println!("p1: {p1}\np2: {p2}");
