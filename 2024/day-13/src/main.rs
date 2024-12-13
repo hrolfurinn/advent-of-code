7dc4864c28c4b282af01034cb9dbcd230fda3534
hrolfurinn <hrolfurinn@gmail.com>
2024-12-13 19:30:01 +0900

    Modularize

--- 2024/day-13/src/main.rs
+++ 2024/day-13/src/main.rs
@@ -4,6 +4,8 @@ use std::fs::read_to_string;
 use std::io::{Read, Result};
 use std::ops::{Index, IndexMut};
 
+type Point = (i64, i64);
+
 fn get_gcd(mut a: i64, mut b: i64) -> i64 {
     while b != 0 {
         let t = b;
@@ -11,6 +13,48 @@ fn get_gcd(mut a: i64, mut b: i64) -> i64 {
         a = t;
     }
     return a;
+}
+
+fn solve_linear_eq(a: Point, b: Point, x: Point) -> (i64, i64) {
+    let mut n = 0;
+    let mut m = 0;
+
+    // First we deal with the case where the vectors are linearly dependent
+    if a.1 * b.0 == a.0 * b.1 {
+        // Checks x/y ratio, i.e. if the angle is the same
+        // If X is not of the same angle, it's unreachable and we continue
+        if a.1 * x.0 == a.0 * x.1 {
+            // We only care about one coordinate, since the angles are all the same
+            let smaller = a.0.min(b.0);
+            let larger = a.0.max(b.0);
+            let gcd = get_gcd(smaller, larger);
+            let needed_parts = x.0 / gcd;
+            let num_larger = gcd * needed_parts / larger;
+            if (x.0 % (num_larger * larger) % smaller) == 0 {
+                if larger == a.0 {
+                    n = num_larger;
+                    m = (x.0 % (num_larger * larger)) / smaller;
+                } else {
+                    n = (x.0 % (num_larger * larger)) / smaller;
+                    m = num_larger;
+                }
+            }
+        }
+    } else {
+        // We can apply the solution
+        // N = D^{-1} X
+        // i.e.
+        // N = (1/det(D)) * adj(D) * X
+        // n = (1/det(D)) ( x_1*b_2 - x_2*b_1 )
+        // m = (1/det(D)) ( x_2*a_1 - x_1*a_1 )
+        let det = (a.0 * b.1) - (a.1 * b.0);
+        let adj_x = ((x.0 * b.1) - (x.1 * b.0), (x.1 * a.0) - (x.0 * a.1));
+        if adj_x.0 % det == 0 && adj_x.1 % det == 0 {
+            n = adj_x.0 / det;
+            m = adj_x.1 / det;
+        }
+    }
+    (n,m)
 }
 
 fn main() -> Result<()> {
@@ -28,7 +72,7 @@ fn main() -> Result<()> {
                 line.split(|c: char| !c.is_digit(10))
                     .filter(|segment| !segment.is_empty())
                     .map(|num| num.parse::<i64>().unwrap())
-                    .collect_tuple::<(i64,i64)>()
+                    .collect_tuple::<(i64, i64)>()
                     .unwrap()
             })
             .collect_tuple()
@@ -40,94 +84,20 @@ fn main() -> Result<()> {
         //
         // We can rewrite the equation in matrix form
         // D * N = X
-        // Where D is the direction matrix (A B), i.e. with columns as our possible directions, 
+        // Where D is the direction matrix (A B), i.e. with columns as our possible directions,
         // and N = (n // m) is a column vector with rows n and m.
         //
         // We have a closed form solution when the A and B are independent.
 
-        let mut n = 0;
-        let mut m = 0;
-
-        // First we deal with the case where the vectors are linearly dependent
-        if a.1 * b.0 == a.0 * b.1 { // Checks x/y ratio, i.e. if the angle is the same
-            // If X is not of the same angle, it's unreachable and we continue
-            if a.1 * x.0 == a.0 * x.1 {
-                // We only care about one coordinate, since the angles are all the same
-                let smaller = a.0.min(b.0);
-                let larger = a.0.max(b.0);
-                let gcd = get_gcd(smaller,larger);
-                let needed_parts = x.0 / gcd;
-                let num_larger = gcd* needed_parts / larger;
-                if (x.0 % (num_larger * larger) % smaller) == 0 {
-                    if larger == a.0 {
-                        n = num_larger;
-                        m = (x.0 % (num_larger * larger)) / smaller;
-                    } else {
-                        n = (x.0 % (num_larger * larger)) / smaller;
-                        m = num_larger;
-                    }
-                    p1 += (n * 3) + m; 
-                }
-            }
-        } else {
-            // We can apply the solution
-            // N = D^{-1} X
-            // i.e.
-            // N = (1/det(D)) * adj(D) * X
-            // n = (1/det(D)) ( x_1*b_2 - x_2*b_1 )
-            // m = (1/det(D)) ( x_2*a_1 - x_1*a_1 )
-            let det = (a.0 * b.1) - (a.1 * b.0);
-            let adj_x = ((x.0 * b.1) - (x.1 * b.0), (x.1 * a.0) - (x.0 * a.1));
-            if adj_x.0 % det == 0 && adj_x.1 % det == 0 {
-                n = adj_x.0 / det;
-                m = adj_x.1 / det;
-                p1 += (n * 3) + m;
-            }
-        }
-
+        let (n,m) = solve_linear_eq(a,b,x);
+        p1 += n*3 + m;
         // Part 2 works the same but we change x
-        
+
         let x = (x.0 + 10000000000000, x.1 + 10000000000000);
 
-        let mut n = 0;
-        let mut m = 0;
+        let (n,m) = solve_linear_eq(a,b,x);
+        p2 += n*3 + m;
 
-        // First we deal with the case where the vectors are linearly dependent
-        if a.1 * b.0 == a.0 * b.1 { // Checks x/y ratio, i.e. if the angle is the same
-            // If X is not of the same angle, it's unreachable and we continue
-            if a.1 * x.0 == a.0 * x.1 {
-                // We only care about one coordinate, since the angles are all the same
-                let smaller = a.0.min(b.0);
-                let larger = a.0.max(b.0);
-                let gcd = get_gcd(smaller,larger);
-                let needed_parts = x.0 / gcd;
-                let num_larger = gcd* needed_parts / larger;
-                if (x.0 % (num_larger * larger) % smaller) == 0 {
-                    if larger == a.0 {
-                        n = num_larger;
-                        m = (x.0 % (num_larger * larger)) / smaller;
-                    } else {
-                        n = (x.0 % (num_larger * larger)) / smaller;
-                        m = num_larger;
-                    }
-                    p2 += (n * 3) + m; 
-                }
-            }
-        } else {
-            // We can apply the solution
-            // N = D^{-1} X
-            // i.e.
-            // N = (1/det(D)) * adj(D) * X
-            // n = (1/det(D)) ( x_1*b_2 - x_2*b_1 )
-            // m = (1/det(D)) ( x_2*a_1 - x_1*a_1 )
-            let det = (a.0 * b.1) - (a.1 * b.0);
-            let adj_x = ((x.0 * b.1) - (x.1 * b.0), (x.1 * a.0) - (x.0 * a.1));
-            if adj_x.0 % det == 0 && adj_x.1 % det == 0 {
-                n = adj_x.0 / det;
-                m = adj_x.1 / det;
-                p2 += (n * 3) + m;
-            }
-        }
     }
 
     println!("p1: {p1}\np2: {p2}");
